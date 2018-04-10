5d19b3703874bc6ab9c4e0e2acb091e1f319f849
Szymon Nowak <nowszy94@gmai.com>
2018-04-10 19:18:45 +0200

    refactor

--- src/main/java/com/sda/service/StringCalculator.java
+++ src/main/java/com/sda/service/StringCalculator.java
@@ -2,15 +2,21 @@ package com.sda.service;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
+
 public class StringCalculator {
     public int calculate(String text) {
         String textWithoutWhitespaces = StringUtils.deleteWhitespace(text);
         String[] elements = StringUtils.split(textWithoutWhitespaces, ";");
         elements = elements == null ? new String[0] : elements;
-        int result = 0;
-        for (String element : elements) {
-            result += Integer.valueOf(element);
-        }
-        return result;
+        return Arrays.stream(elements)
+//                .mapToInt(new ToIntFunction<String>() {
+//                    @Override
+//                    public int applyAsInt(String value) {
+//                        return Integer.valueOf(value);
+//                    }
+//                })
+                .mapToInt(value -> Integer.valueOf(value)) //to robi to samo co wyzej zakomentowany kod
+                .sum();
     }
 }
