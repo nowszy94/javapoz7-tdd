233d858ec5cc115df3b56a1fbdcd0a080b146ce7
Szymon Nowak <nowszy94@gmai.com>
2018-04-10 18:46:18 +0200

    calculation of blank string

--- src/main/java/com/sda/service/StringCalculator.java
+++ src/main/java/com/sda/service/StringCalculator.java
@@ -1,7 +1,12 @@
 package com.sda.service;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class StringCalculator {
     public int calculate(String text) {
+        if (StringUtils.isBlank(text)) {
+            return 0;
+        }
         String[] split = text.split(";");
         return Integer.valueOf(split[0]) + Integer.valueOf(split[1]);
     }
--- src/test/java/com/sda/service/StringCalculatorTest.java
+++ src/test/java/com/sda/service/StringCalculatorTest.java
@@ -1,16 +1,40 @@
 package com.sda.service;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalculatorTest {
 
+    private StringCalculator calculator;
+
+    @Before
+    public void init() {
+        this.calculator = new StringCalculator();
+    }
+
+    @Test
+    public void testCalculationForBlankValue() {
+        //given
+        String value = "";
+        //when
+        int result = calculator.calculate(value);
+        //then
+        Assert.assertEquals(0, result);
+    }
+
+    @Test
+    public void testCalculationForNullValue() {
+        //given
+        String value = null;
+        //when
+        int result = calculator.calculate(value);
+        //then
+        Assert.assertEquals(0, result);
+    }
+
     @Test
     public void testSimpleCalculation() {
-        //given
-        StringCalculator calculator = new StringCalculator();
-
-        //then
         Assert.assertEquals(5, calculator.calculate("2;3"));
         Assert.assertEquals(6, calculator.calculate("1;5"));
         Assert.assertEquals(17, calculator.calculate("8;9"));
