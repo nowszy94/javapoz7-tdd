08a738bf0276b686fc336b258babc22a4411dcce
Szymon Nowak <nowszy94@gmai.com>
2018-04-11 18:57:59 +0200

    introduced cucumber parametrized steps

--- src/test/java/com/sda/service/cucumber/StringCalculatorSteps.java
+++ src/test/java/com/sda/service/cucumber/StringCalculatorSteps.java
@@ -21,34 +21,14 @@ public class StringCalculatorSteps {
         this.calculator = new StringCalculator();
     }
 
-    @Given("^I pass single number value$")
-    public void I_pass_single_number_value() {
-        this.value = "5";
-    }
-
-    @And("^I pass null value$")
+    @And("^I pass value null$")
     public void I_pass_null_value() {
         this.value = null;
     }
 
-    @And("^I pass empty value$")
-    public void I_pass_empty_value() {
-        this.value = "";
-    }
-
-    @And("^I pass two numbers value$")
-    public void I_pass_two_numbers_value() {
-        this.value = "2;7";
-    }
-
-    @And("^I pass multiple numbers value$")
-    public void I_pass_multiple_numbers_value() {
-        this.value = "2;3;4";
-    }
-
-    @And("^I pass multiple numbers with whitespaces value$")
-    public void I_pass_multiple_numbers_with_whitespaces_value() {
-        this.value = "2   ;3        ; \n4";
+    @Given("^I pass (.*) value$")
+    public void I_pass_single_number_value(String text) {
+        this.value = text;
     }
 
     @When("^I trigger calculate function$")
@@ -56,18 +36,8 @@ public class StringCalculatorSteps {
         this.result = calculator.calculate(value);
     }
 
-    @Then("^I get 5 as a result$")
-    public void I_get_5_as_a_result() {
-        Assert.assertEquals(5, result);
-    }
-
-    @Then("^I get 0 as a result$")
-    public void I_get_0_as_a_result() {
-        Assert.assertEquals(0, result);
-    }
-
-    @Then("^I get 9 as a result$")
-    public void I_get_9_as_a_result() {
-        Assert.assertEquals(9, result);
+    @Then("^I get (.*) as a result$")
+    public void I_get_5_as_a_result(int passedResult) {
+        Assert.assertEquals(passedResult, result);
     }
 }
--- src/test/resources/com/sda/service/cucumber/stringCalculator.feature
+++ src/test/resources/com/sda/service/cucumber/stringCalculator.feature
@@ -1,36 +1,36 @@
 Feature:
   Scenario: I can calculate single number
     Given I initialize stringCalculator
-    And I pass single number value
+    And I pass 5 value
     When I trigger calculate function
     Then I get 5 as a result
 
   Scenario: I can calculate null number
     Given I initialize stringCalculator
-    And I pass null value
+    And I pass value null
     When I trigger calculate function
     Then I get 0 as a result
 
   Scenario: I can calculate empty number
     Given I initialize stringCalculator
-    And I pass empty value
+    And I pass  value
     When I trigger calculate function
     Then I get 0 as a result
 
   Scenario: I can calculate two number
     Given I initialize stringCalculator
-    And I pass two numbers value
+    And I pass 5;4 value
     When I trigger calculate function
     Then I get 9 as a result
 
   Scenario: I can calculate multiple numbers
     Given I initialize stringCalculator
-    And I pass multiple numbers value
+    And I pass 2;3;4 value
     When I trigger calculate function
     Then I get 9 as a result
 
   Scenario: I can calculate multiple numbers with whitespaces
     Given I initialize stringCalculator
-    And I pass multiple numbers with whitespaces value
+    And I pass 2  ;3    ;  4   value
     When I trigger calculate function
     Then I get 9 as a result
